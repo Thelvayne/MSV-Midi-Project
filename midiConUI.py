8c6f5a28e3d65f7190a1ff6b39659817bd03d0cd
Philipp Jäckel <philipp.jaeckel.1998@gmail.com>
2024-02-13 14:36:14 +0100

    UI changes:
     panel size depending on  loaded midi file
     panels in scrollable container for panel size > window
     panels have panels that show notes
     adapted delete methods to work with changes

--- midiConUI.py
+++ midiConUI.py
@@ -13,7 +13,7 @@ HEIGHT-=90
 WIDTH-=10
 SCREEN = pygame.display.set_mode((WIDTH, HEIGHT), pygame.RESIZABLE)
 
-# Initiating alle UI Managing Tools
+# Initiating all UI Managing Tools
 CLOCK = pygame.time.Clock() # --> This will be relevant for Playback speed and could cause conflicts with midi tempo/bpm
 MANAGER = pygame_gui.UIManager((WIDTH, HEIGHT), 'theme.json')
 
@@ -35,8 +35,6 @@ CONVERTTOWAVBUTTON = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((160
                                                   manager=MANAGER,
                                                   anchors={"left":"left","top":"top"})
 
-MIDIFILE = None
-
 MIDIFILENAME = pygame_gui.elements.UILabel(pygame.Rect((CONVERTTOWAVBUTTON.relative_rect.right + 10,10),(-1,-1)),
                                            text="",
                                            visible=1,
@@ -54,35 +52,80 @@ def exitApp():
     pygame.quit()
     sys.exit()
 
-LASTCREATEDPANELNUMBER = -1
-def setLastCreatedPanelNumber(number):
-    global LASTCREATEDPANELNUMBER
-    LASTCREATEDPANELNUMBER = number
-
 def createUIPanels(filetracks):
     i = 0
-    for tracks in filetracks:
-        SCREEN.fill(pygame.Color("black"), (ADDCOLUMNBUTTON.relative_rect.left, 
-                                            ADDCOLUMNBUTTON.relative_rect.top, 
-                                            ADDCOLUMNBUTTON.relative_rect.right - MIDIFILENAME.relative_rect.left, 
-                                            ADDCOLUMNBUTTON.relative_rect.bottom - MIDIFILENAME.relative_rect.top))
 
-        name = "SYSTEMCOLUMN" + str(i)
-        name = pygame_gui.elements.UIPanel(pygame.Rect((10,60+i*90),(WIDTH-20,90)),
+    length = 0
+    for item in filetracks[0]:
+        if hasattr(item, 'time'):
+            length += item.time
+    
+    print(length)
+
+    amountPanels = len(filetracks) - 1
+
+    scrollableContainer_name = "CONTAINER" + str(i)
+    scrollableContainer_left_top = (10,60)
+    scrollableContainer_width_height = (WIDTH-20,amountPanels*100+10)
+    scrollableContainer_rect = pygame.Rect(scrollableContainer_left_top, scrollableContainer_width_height)
+
+    scrollableContainer = pygame_gui.elements.UIScrollingContainer(
+        relative_rect=scrollableContainer_rect,
+        manager=MANAGER,
+        starting_height=1,
+        object_id=scrollableContainer_name,
+        anchors={"left":"left","top":"top"}
+    )
+
+    SCREEN.fill(pygame.Color("black"), (ADDCOLUMNBUTTON.relative_rect.left, 
+                                        ADDCOLUMNBUTTON.relative_rect.top, 
+                                        ADDCOLUMNBUTTON.relative_rect.right - ADDCOLUMNBUTTON.relative_rect.left, 
+                                        ADDCOLUMNBUTTON.relative_rect.bottom - ADDCOLUMNBUTTON.relative_rect.top))
+    
+    while i < len(filetracks) - 1:
+        
+        panel_name = "PANEL" + str(i)
+        panel_left_top = (0,i * 100)
+        panel_width_height = (length,90)
+        panel_rect = pygame.Rect(panel_left_top, panel_width_height)
+        
+        panel = pygame_gui.elements.UIPanel(
+            relative_rect=panel_rect,
+            starting_height=2,
             manager=MANAGER,
-            object_id=name,
+            parent_element=scrollableContainer,
+            container=scrollableContainer,
+            object_id=panel_name,
             anchors={"left":"left","top":"top"})
-        ADDCOLUMNBUTTON.set_relative_position((WIDTH/2, name.relative_rect.bottom + 10))
-        setLastCreatedPanelNumber(i)
+        
         i += 1
-        if i == len(filetracks) - 1:
-            break
+    
+    ADDCOLUMNBUTTON.set_relative_position((WIDTH/2, scrollableContainer.relative_rect.bottom + 10))
 
-def getPanel(number):
-    Panel = "SYSTEMCOLUMN" + str(number)
+def setScrollableDimensions(Width, Height):
+    i = 0
+    while getContainer(i) != None:
+        scrollableContainer = getContainer(i)
+        scrollableContainer.set_scrollable_area_dimensions((Width, Height))
+        i += 1
+
+def getContainer(number):
+    scrollableContainer = "CONTAINER" + str(number)
     for element in MANAGER.get_root_container().elements:
+        if isinstance(element, pygame_gui.elements.UIScrollingContainer):
+            if element.object_ids[0] == scrollableContainer:
+                return element
+    print(f"No UIScrollingContainer found with object_ids: {scrollableContainer}")  # <-- add this line
+    return None
+            
+def getPanel(containernr, panelnr):
+    panel = "PANEL" + str(panelnr)
+    container = getContainer(containernr)
+    if container == None:
+        return None
+    for element in container.scrollable_container.elements:
         if isinstance(element, pygame_gui.elements.UIPanel):
-            if element.object_ids[0] == Panel:
+            if element.object_ids[1] == panel:
                 return element
 
 def removeLabeltext():
@@ -97,18 +140,21 @@ def removeOldUIElements():
     removeLabeltext()
 
     # löscht alte Panels
-    global LASTCREATEDPANELNUMBER
-    if LASTCREATEDPANELNUMBER >= 0:
-        i = 0
-        while i <= LASTCREATEDPANELNUMBER:
-            panel = getPanel(i)
-            if panel != None:
-                panel.kill()
-                SCREEN.fill(pygame.Color('black'), (panel.relative_rect.left,
-                                                panel.relative_rect.top,
-                                                panel.relative_rect.right - panel.relative_rect.left,
-                                                panel.relative_rect.bottom - panel.relative_rect.top))
-            i += 1
+    i = 0
+    container = getContainer(i)
+    while container != None:
+        panel = getPanel(containernr=0, panelnr=i)
+        if panel == None:
+            break
+        panel.kill()
+        SCREEN.fill(pygame.Color('black'), (container.relative_rect.left,
+                                                container.relative_rect.top,
+                                                container.relative_rect.right - container.relative_rect.left,
+                                                container.relative_rect.bottom - container.relative_rect.top))
+        i += 1
+
+    if container != None:
+        container.kill()
 
     # verschiebt +-Button
     SCREEN.fill(pygame.Color('black'), (ADDCOLUMNBUTTON.relative_rect.left,
@@ -116,6 +162,87 @@ def removeOldUIElements():
                                         ADDCOLUMNBUTTON.relative_rect.right - ADDCOLUMNBUTTON.relative_rect.left,
                                         ADDCOLUMNBUTTON.relative_rect.bottom - ADDCOLUMNBUTTON.relative_rect.top))
     ADDCOLUMNBUTTON.set_relative_position((WIDTH/2,180))
+
+def drawNotes(tracks):
+    i = 0
+    while i < len(tracks) - 1:
+        panel = getPanel(containernr=0, panelnr=i)
+
+        track = tracks[i+1]
+
+        offset = 0
+        for item in track:
+            if item.type == 'program_change':
+                offset = item.time
+                break
+        print(offset)
+        i += 1
+
+        current_position = offset
+
+        n = 0
+        while n < len(track):
+            modi = track[n]
+            if modi.type == 'note_on':
+                current_position += track[n].time
+                x = current_position
+                y = get_y_placement_for_note(track[n].note)
+                length = track[n+1].time
+                n += 2
+                current_position += length
+
+                note_position = (x,y)
+                note_size = (length, 10)
+                note_rect = pygame.Rect(note_position,note_size)
+
+                note = pygame_gui.elements.UIPanel(
+                    relative_rect=note_rect,
+                    starting_height=3,
+                    manager=MANAGER,
+                    parent_element=panel,
+                    container=panel,
+                    anchors={"left":"left","top":"top"} 
+                )
+            else:
+                n += 1
+
+def get_y_placement_for_note(note):
+    if note == 67:
+        return 0
+    elif note == 68:
+        return 5
+    elif note == 69:
+        return 10
+    elif note == 70:
+        return 15
+    elif note == 71:
+        return 20
+    elif note == 72:
+        return 25
+    elif note == 73:
+        return 30 
+    elif note == 74:
+        return 35 
+    elif note == 75:
+        return 40 
+    elif note == 76:
+        return 45 
+    elif note == 77:
+        return 50 
+    elif note == 78:
+        return 55                
+    elif note == 79:
+        return 60 
+    elif note == 80:
+        return 65 
+    elif note == 81:
+        return 70 
+    elif note == 82:
+        return 75 
+    elif note == 83:
+        return 80
+    else:
+        return 85
 
 def app():
     MIDIFILE=None
@@ -127,13 +254,20 @@ def app():
             if event.type == pygame_gui.UI_BUTTON_PRESSED and event.ui_element == LOADFILEBUTTON:
                 
                 import MidiFileLoader,Partiture
-                from mido import Message,MetaMessage
+                #from mido import Message,MetaMessage
+                import mido
 
                 MIDIFILE = MidiFileLoader.loadMidiFile()
                 #PARTITURE:Partiture.Partiture = Partiture.Partiture(MIDIFILE)
                 if MIDIFILE is not None:
+
                     MIDIFILE.print_tracks()
+                    tracks = MIDIFILE.tracks
+
+                    # UI Part:
+                    # clean up screen
                     removeOldUIElements()
+
                     # get loaded file name
                     FILENAME = MIDIFILE.filename
                     POSITIONSLASH = FILENAME.rfind("\\")
@@ -141,6 +275,16 @@ def app():
 
                     # dynamic creation for UIPanels to show different channels
                     createUIPanels(MIDIFILE.tracks)
+                    container_width = 0
+                    for item in tracks[0]:
+                        if hasattr(item, 'time'):
+                            container_width += item.time
+                    amount_panels = (len(MIDIFILE.tracks) - 1)
+                    container_height = (amount_panels * 90) + ((amount_panels - 1) * 10)
+                    setScrollableDimensions(container_width,container_height)
+                    updateDisplay()
+                    drawNotes(tracks)
+
                 else: 
                     removeLabeltext()
                     MIDIFILENAME.set_text(f"File not found or cancelled")
